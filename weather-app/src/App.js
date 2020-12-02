7f8e6afc00dd53ce4797419a17b7b7e2990555ff
Amanda <acheng742@gmail.com>
2020-12-02 13:45:55 -0500

    Add components

---
+++ weather-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Titles from './Components/Titles';
+import Form from './Components/Form';
+import Weather from './Components/Weather';
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				<Titles />
+				<Form />
+				<Weather />
+			</div>
+		);
+	}
+};
+
+export default App;
